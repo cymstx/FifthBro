bb3fca75a852e5762fa36898d80d34339faf25e6
gnnort <ngtron25@gmail.com>
2022-04-13 02:04:19 +0800

    QR gen now displays as dialog box

--- app/src/main/java/com/example/testbro/ActivityQRGen.java
+++ app/src/main/java/com/example/testbro/ActivityQRGen.java
@@ -1,9 +1,12 @@
 package com.example.testbro;
 
+import android.app.Activity;
 import android.graphics.Bitmap;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.view.ViewGroup;
+import android.view.Window;
 import android.widget.ImageView;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.annotation.NonNull;
@@ -24,7 +27,7 @@ import com.google.firebase.database.ValueEventListener;
 
 import net.glxn.qrgen.android.QRCode;
 
-public class ActivityQRGen extends AppCompatActivity{
+public class ActivityQRGen extends Activity {
 
     String itemID;
     DatabaseReference dbref;
@@ -33,7 +36,9 @@ public class ActivityQRGen extends AppCompatActivity{
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.activity_qrgenerator);
+        getWindow().setLayout(ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT);
         extras = getIntent().getExtras();
         itemID = extras.getString("item");
         Log.d("item id", itemID);
